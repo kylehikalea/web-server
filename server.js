5013375b04712f21b1b48cc1d3e16c69d8159736
Kyle Hikalea <hikalea@gmail.com>
2016-01-04 14:39:59 -0800

    Added html to /about page

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res) {
-	res.send('About Us')
+	res.send('<h1>About Us</h1>')
 })
 
 app.use(express.static(__dirname + '/public'));
